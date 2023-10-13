115c525b9e2cf4fb726dddd41eea6a4193ea4b01
Brayan2789 <147455212+Brayan2789@users.noreply.github.com>
2023-10-12 20:25:54 -0600

    Adición de javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop2","l":"FigurasGeometricas"},{"p":"poop2","l":"POOP2"},{"p":"poop2","l":"SumaPares"}];updateSearchResults();
